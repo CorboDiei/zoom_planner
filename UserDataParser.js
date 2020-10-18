29e3d2b2989e819a4683a99bb370998cf0257cf3
Alena Dickmann <adickm@uw.edu>
2020-10-17 23:29:23 -0700

    finished(?) UserDataParser.js

--- UserDataParser.js
+++ UserDataParser.js
@@ -1,9 +1,40 @@
-const fs = require('fs');
+const { once } = require('events');
+const { createReadStream } = require('fs');
+const { createInterface } = require('readline');
+let userData = '';
+let zoomEntries = '';
 
-function getUserData(fileName) {
-    fs.readFile(fileName, function (err, data) {
-        if (err) throw err;
-        console.log(data.toString());
-    });
+async function processLineByLine(fileName) {
+    try {
+        const rl = createInterface({
+            input: createReadStream(fileName),
+            crlfDelay: Infinity
+        });
+
+        rl.on('line', (line) => {
+            // Process the line.
+            userData += line + "\n";
+        });
+
+        await once(rl, 'close');
+
+        console.log('File processed.');
+    } catch (err) {
+        console.error(err);
+    }
 }
-getUserData("user_pXLxAiHUJK4Bgb3PF4oAzbyS4OtUs2PkVVu7cKWb (1).ics");
+
+async function processFile(fileName) {
+    await processLineByLine('testCal.ics');
+    let userDataArray = userData.split("BEGIN:VEVENT");
+    // parses through the entire file by event
+    for (let i = 1; i < userDataArray.length; i++) {
+        if (userDataArray[i].includes('washington.zoom.us')) {
+            zoomEntries += userDataArray[i];
+        }
+    }
+    console.log(zoomEntries);
+}
+
+processFile('testCal.ics');
+
